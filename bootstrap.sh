68bb9fe0a5e65fad07b5daac4820b94def283ea3
tats-faire <tatianahetzel@gmx.net>
2023-11-20 20:16:44 +0100

    add script to fetch Libft files directly from the remote project

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+svn export https://github.com/tats-faire/Libft/trunk/ft_strlen.c Libft/ft_strlen.c
+svn export https://github.com/tats-faire/Libft/trunk/ft_itoa.c Libft/ft_itoa.c
+svn export https://github.com/tats-faire/Libft/trunk/ft_putchar_fd.c Libft/ft_putchar_fd.c
+svn export https://github.com/tats-faire/Libft/trunk/ft_putstr_fd.c Libft/ft_putstr_fd.c
+svn export https://github.com/tats-faire/Libft/trunk/ft_putnbr_fd.c Libft/ft_putnbr_fd.c
